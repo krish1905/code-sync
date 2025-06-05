fe68560c1f8c699ae3c0f3359d9f9456395cb009
krish <46093804+krish1905@users.noreply.github.com>
2025-06-05 19:31:21 -0400

    Update solution for two-sum

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,8 +1,18 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; ++i) {
+        for (int j = i + 1; j < numsSize; ++j) {
+            if (nums[i] + nums[j] == target) {
+                int* res = (int*)malloc(2 * sizeof(int));
+                res[0] = i;
+                res[1] = j;
+                *returnSize = 2;
+                return res;
+            }
+        }
+    }
+    *returnSize = 0; // no solution found
+    return NULL;
+}
