5b5511322a17559e5dfc2b187a78d0db4f3e11a1
krish <46093804+krish1905@users.noreply.github.com>
2025-06-05 19:44:39 -0400

    Add solution for two-sum

---
+++ leetcode/two-sum.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (map.count(complement)) {
+                return {map[complement], i};
+            }
+            map[nums[i]] = i;
+        }
+        return {}; // Problem guarantees a solution exists
+    }
+};
