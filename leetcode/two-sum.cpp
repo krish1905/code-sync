9458a126304be163b661109bcf3a0b25717c1db4
krish <46093804+krish1905@users.noreply.github.com>
2025-06-05 19:53:19 -0400

    Update solution for two-sum

--- leetcode/two-sum.cpp
+++ leetcode/two-sum.cpp
@@ -8,9 +8,9 @@ public:
         unordered_map<int, int> map;
         for (int i = 0; i < nums.size(); ++i) {
             int complement = target - nums[i];
-            if (map.count(complement)) {
+            if (map.count(complement)) {ncrn
                 return {map[complement], i};
-            }
+            }nknkn
             map[nums[i]] = i;
         }
         return {}; // Problem guarantees a solution exists
